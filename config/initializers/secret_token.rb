9d32c66c9f05c6ba47ee0e60b72f9eadba622a1e
Jurgens Smit <contact@jurgenssmit.com>
2013-05-22 10:15:03 +1000

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Testingaws::Application.config.secret_token = 'db5fdf200bec47cf06ff2f76e6a6871a680551d9d44c0e583890ef3aebd9bc950bb80a4fcc0e3fc27537fc6769e685c8245dfd9c6a30479029e6e2fc28263112'
